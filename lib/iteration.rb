75a4b2671472417293d50615125e24fa22b25be4
Robert Notwicz <rob.notwicz@gmail.com>
2020-01-15 07:21:24 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,26 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+  joined = []
+  src.each do |arr|
+    joined.push("I love #{arr[0]} and #{arr[1]} on my pizza")
+  end
+  return joined
 end
 
-def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+
+def find_greater_pair(src)  
+  greatest = []
+  src.each do |arr|
+    greatest.push(arr.max)
+  end
+  return greatest
 end
 
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+  total = 0
+  src.each do |arr|
+    if arr[0] %2 == 0 && arr[1] %2 == 0 then
+      total += arr[0] + arr[1]
+    end
+  end
+  return total
 end
